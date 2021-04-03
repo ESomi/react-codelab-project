fab64fc503b2880e8f305985f2327b67d29ca236
ESomi <sweetsom27@gmail.com>
2021-04-04 01:22:02 +0900

    until npm run development (app crashed error)

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'babel-polyfill',
+        './src/index.js',
+        './src/style.css'
+    ],
 
     output: {
         path: __dirname + '/public/',
         filename: 'bundle.js'
     },
 
-    devServer: {
-        hot: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 4000,
-        contentBase: __dirname + '/public/',
-    },
-
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                loaders: ['babel?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 })],
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader'
             }
         ]
     },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+    resolve: {
+        root: path.resolve('./src')
+    },
+
+    plugins:[
+        new webpack.DefinePlugin({
+          'process.env':{
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress:{
+            warnings: true
+          }
+        })
     ]
+
 };
